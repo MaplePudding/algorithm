f92def1b6880955ea9c64eefbad9610a00be3692
MaplePudding <fengtangbuding@gmail.com>
2020-08-04 11:30:15 +0800

    commit

--- leetcode/c++/695 Max Area of Island.cpp
+++ leetcode/c++/695 Max Area of Island.cpp
@@ -1,33 +1,30 @@
 class Solution {
 public:
-    int dfs(vector<vector<int>>& grid, int a, int b){
-        
-        int s = 0;
-        if(a < 0 || a >= grid.size() || b < 0 || b >= grid[0].size()){
-            return 0;
-        }
-        if(grid[a][b] == 1){
-            ++s;
-            grid[a][b] = 0;
-        }else{
-            return 0;
-        }
-        s += dfs(grid, a + 1, b);
-        s += dfs(grid, a - 1, b);
-        s += dfs(grid, a, b - 1);
-        s += dfs(grid, a, b + 1);
-        return s;
-    }
+    vector<vector<int> > d={{1,-1,0,0},{0,0,1,-1}};
+        int n,m,curr=0,ans=0;
+
     
+    void dfs(int x,int y,vector<vector<int> > &g){
+        if(x<0 || y<0 || x>=n || y>=m || g[x][y]==2 || g[x][y]==0) return;
+        g[x][y]=2;
+        curr++;
+        for(int i=0;i<4;i++)
+        {
+            dfs(x+d[0][i],y+d[1][i],g);
+        }
+    }
     int maxAreaOfIsland(vector<vector<int>>& grid) {
-        int res = 0;
-        for(int a = 0; a < grid.size(); ++a){
-            for(int b = 0; b < grid[0].size(); ++b){
-                if(grid[a][b] == 1){
-                    res = max(dfs(grid, a, b), res);
+        n=grid.size();
+        if(n==0) return 0;
+        m=grid[0].size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==1){
+                    curr=0;
+                    dfs(i,j,grid);
+                    ans=max(ans,curr);
                 }
             }
-        }
-        return res;
+        }return ans;
     }
 };
